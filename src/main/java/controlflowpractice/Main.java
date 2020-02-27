42e5c21ac219d99c9b4d5ac94d24961720e89ffe
sgharai <sofiagharaibeh@gmail.com>
2020-02-27 10:14:20 -0500

    adds control flow exercises

---
+++ src/main/java/controlflowpractice/Main.java
@@ -0,0 +1,44 @@
+package controlflowpractice;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean isTrue = true;
+
+        if(isTrue) {
+            System.out.println("This is running because isTrue is true.");
+        }
+
+        int[] ints = {1, 2, 3};
+
+        for(int integer : ints){
+            System.out.println(integer + 2);
+        }
+
+        int lattes = 0;
+        do {
+            System.out.println(lattes  + " lattes consumed.");
+            lattes++;
+        } while (lattes < 10);
+
+        String villain = "Hades";
+        switch(villain) {
+            case "Sauron":
+                System.out.println("Okay");
+                break;
+            case "Loki":
+                System.out.println("Meh");
+                break;
+            case "Hades":
+                System.out.println("Bada bing bada boom");
+                break;
+            default:
+                System.out.println("I need a hero");
+                break;
+
+
+
+        }
+
+
+    }
+}
